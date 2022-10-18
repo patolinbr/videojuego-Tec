16ad0a10ec0b65ab25b952173e39b217c8801b88
Hiram Muñoz <hirammunozr@outlook.com>
2022-10-18 13:30:41 -0500

    ui changes

--- server/server/Controllers/HomeController.cs
+++ server/server/Controllers/HomeController.cs
@@ -15,12 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
+        return RedirectToAction("Index", "Course");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
